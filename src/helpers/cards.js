90d7129bdd151401bd54b46bbf87f3576afdcaf9
Fahim Bercha <fhyembercha@gmail.com>
2020-04-10 16:46:02 +0500

    IAdded the Fisher Yates Shuffle method to the cards.js in Helpers.

--- src/helpers/cards.js
+++ src/helpers/cards.js
@@ -1,96 +1,93 @@
-const deck = ["d1", "d2", "d3", "h1", "h2", "h3", "s1", "s2", "s3", "c1", "c2", "c3"]
+const suits = ["H", "S", "C", "D"];
+const numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
 
-// const cardDistributor = (cardsDeck) => {
-//   //logic to randomly generate the distributed array
-//   const returnDeck = { firstPlayer: [cardsDeck[0], cardsDeck[5], cardsDeck[7]], secondPlayer: [cardsDeck[2], cardsDeck[5]] }
-//   console.log(returnDeck);
-//   return returnDeck;
-// }
+const deck = [
+  "HA",
+  "H2",
+  "H3",
+  "H4",
+  "H5",
+  "H6",
+  "H7",
+  "H8",
+  "H9",
+  "H10",
+  "HJ",
+  "HQ",
+  "HK",
+  "SA",
+  "S2",
+  "S3",
+  "S4",
+  "S5",
+  "S6",
+  "S7",
+  "S8",
+  "S9",
+  "S10",
+  "SJ",
+  "SQ",
+  "SK",
+  "CA",
+  "C2",
+  "C3",
+  "C4",
+  "C5",
+  "C6",
+  "C7",
+  "C8",
+  "C9",
+  "C10",
+  "CJ",
+  "CQ",
+  "CK",
+  "DA",
+  "D2",
+  "D3",
+  "D4",
+  "D5",
+  "D6",
+  "D7",
+  "D8",
+  "D9",
+  "D10",
+  "DJ",
+  "DQ",
+  "DK",
+];
 
-// function calculator(a, b, operation) {
-//   let result;
-//   switch (operation) {
-//     case "add": {
-//       result = a + b;
-//       break;
-//     };
-//     case "minus": {
-//       result = a - b;
-//       break;
-//     }
-//     case "multiply": {
-//       result = a * b;
-//       break;
-//     }
-//     case "divide": {
-//       result = a / b;
-//       break;
-//     }
-//   }
-//   return result;
-// }
+//   The Fisher Yates Method
 
-// const answer = calculator(15, 4, "divide");
-// console.log(answer);
-// answer = calculator(15, 4, "add");
-// console.log(answer);
+//   The most popular correct method, is called the Fisher Yates shuffle, and was introduced in data science as early as 1938!
 
+//   In JavaScript the method can be translated to this:
 
-// function cardDistributor_v2(cardsDeck) {
-//   //logic to randomly generate the distributed array
-//   const returnDeck = { firstPlayer: [cardsDeck[0], cardsDeck[5], cardsDeck[7]], secondPlayer: [cardsDeck[2], cardsDeck[5]] }
-//   console.log(returnDeck);
-//   return returnDeck;
-// }
-// cardDistributor_v2(deck);
+for (i = deck.length - 1; i > 0; i--) {
+  j = Math.floor(Math.random() * i);
+  k = deck[i];
+  deck[i] = deck[j];
+  deck[j] = k;
+}
 
-// function friendChanger(friends, newFriend, index) {
-//   friends[index] = newFriend;
-//   return friends
-// };
+console.log(deck);
 
-// const faheemsFriend = { first: "boqo", second: "nasir" }
-// const changedFriends = friendChanger(faheemsFriend, "zahidto", "second");
-// console.log(changedFriends);
+const firstPlayer = deck.slice(0, 13);
+const secondPlayer = deck.slice(13, 26);
+const thirdPlayer = deck.slice(26, 39);
+const fourthPlayer = deck.slice(39, 52);
 
-// const arr = [11, 2, 3];
-// arr.push = () => {
-//   console.log("new push");
-// }
-// arr.push(5);
-// console.log(typeof arr.push);
-
-
-// class Car {
-//   constructor(model, company) {
-//     this.model = model;
-//     this.company = company
-//   }
-//   getModel() {
-//     return this.model
-//   };
-//   getCompany() {
-//     return this.company
-//   }
-// }
-
-// const car1 = new Car("1967", "BMW");
-// const car2 = new Car("2000", "TESLA");
-// const company = car1.getCompany();
-// const company1 = car2.getCompany();
-// console.log(company, company1)
-
-
-// class Array {
-//   constructor(a, b, c) {
-//     this.a = a;
-//     this.b = b;
-//     this.c = c;
-//   }
-//   getValueAtIndex(index) {
-//     return this[index];
-//   }
-// }
-
-// const arr = new Array("a", "b", "c");
-// console.log(arr.getValueAtIndex("a"))
+console.log(firstPlayer, secondPlayer, thirdPlayer, fourthPlayer);
