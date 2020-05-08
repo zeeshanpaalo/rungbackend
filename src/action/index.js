4afc7b6195998c9a29a94ad2014ea9838c089c7f
zeeshanali <zeeshanali@itverticals.com>
2020-05-08 16:19:53 +0500

    added the basic flow almost there

--- src/action/index.js
+++ src/action/index.js
@@ -3,8 +3,13 @@ export const joinRoomAction = {
   success: "JOIN_ROOM_SUCCESS",
   error: "JOIN_ROOM_ERROR"
 }
+export const selectRung = {
+  begin: "SELECT_RUNG_BEGIN",
+  success: "SELECT_RUNG_SUCCESS",
+  error: "SELECT_RUNG_ERROR"
+}
 export const leftRoom = "LEFT_ROOM";
 export const socketConnectionSucess = "CONNECT_TO_SOCKET_SUCCESS";
 export const updatedRoomsEvent = "ROOMS_UPDATED";
 
-export default { joinRoomAction, socketConnectionSucess, updatedRoomsEvent, leftRoom };
+export default { joinRoomAction, socketConnectionSucess, updatedRoomsEvent, leftRoom, selectRung };
--- src/models/Room.js
+++ src/models/Room.js
@@ -1,9 +1,14 @@
 import Mongoose from 'mongoose';
 const Schema = Mongoose.Schema;
+const { ObjectId } = Schema.Types;
 
 const Room = new Schema({
   name: String,
-  players: []
+  players: [],
+  lastRungSelector: ObjectId,
+  ready: {type: Boolean, default: false},
+  color: { type : String, enum: ["HEARTS", "DIAMONDS", "SPADES", "CLUBS", null]},
+  currentTurn: ObjectId
 });
 
 export default Mongoose.model("Room", Room, "rooms")
